459ae859d74b5b99675a22fc550e72586649081c
Aidan Bleser <aidanbleser35@gmail.com>
2025-09-15 17:17:08 -0500

    Update plugin.ts

--- packages/svelte/src/plugin.ts
+++ packages/svelte/src/plugin.ts
@@ -8,7 +8,6 @@ export const plugin = (): Plugin => ({
 	name: "transform-lucide-imports",
 	enforce: "pre",
 	async transform(code, fileName) {
-		console.log(fileName);
 		if (CORE_SUPPORTED_EXTENSIONS.some((ext) => fileName.endsWith(ext))) {
 			return { code: coreTransform(code) };
 		}
