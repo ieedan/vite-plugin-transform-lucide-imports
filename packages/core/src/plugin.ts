2308bca68275434bf0bdd34493af9ad11e71952d
Aidan Bleser <aidanbleser35@gmail.com>
2025-09-15 16:43:32 -0500

    more

--- packages/core/src/plugin.ts
+++ packages/core/src/plugin.ts
@@ -1,16 +1,14 @@
 import type { Plugin } from "vite";
-import { transform } from "./transform";
+import { transform as coreTransform } from "./transform";
 
 export const SUPPORTED_EXTENSIONS = [".ts", ".tsx", ".js", ".jsx", ".mjs"];
 
-type TransformImportOptions = {};
-
-export const plugin = (_: TransformImportOptions = {}): Plugin => ({
+export const plugin = (): Plugin => ({
 	name: "transform-lucide-imports",
-	enforce: 'pre',
-	transform: async (code, fileName) => {
-		if (!SUPPORTED_EXTENSIONS.some((ext) => fileName.endsWith(ext))) return;
+	enforce: "pre",
+	async transform(code, fileName) {
+		if (!SUPPORTED_EXTENSIONS.some((ext) => fileName.endsWith(ext))) return {};
 
-		return transform(code);
+		return { code: coreTransform(code) };
 	},
-})
+});
--- packages/svelte/src/plugin.ts
+++ packages/svelte/src/plugin.ts
@@ -1,21 +1,22 @@
 import type { Plugin } from "vite";
-import { transform, SUPPORTED_EXTENSIONS as CORE_SUPPORTED_EXTENSIONS } from "../../core/dist/index.mjs";
+import { transform as coreTransform, SUPPORTED_EXTENSIONS as CORE_SUPPORTED_EXTENSIONS } from "vite-plugin-transform-lucide-imports";
 import { transform as svelteTransform } from "./transform";
 
 export const SUPPORTED_EXTENSIONS = [".svelte"];
 
-type TransformImportOptions = {};
-
-export const plugin = (_: TransformImportOptions = {}): Plugin => ({
+export const plugin = (): Plugin => ({
 	name: "transform-lucide-imports",
-	enforce: 'pre',
-	transform: async (code, fileName) => {
+	enforce: "pre",
+	async transform(code, fileName) {
+		console.log(fileName);
 		if (CORE_SUPPORTED_EXTENSIONS.some((ext) => fileName.endsWith(ext))) {
-			return transform(code);
+			return { code: coreTransform(code) };
 		}
 
 		if (SUPPORTED_EXTENSIONS.some((ext) => fileName.endsWith(ext))) {
-			return svelteTransform(code);
+			return { code: await svelteTransform(code) };
 		}
+
+		return {};
 	},
-})
+});
