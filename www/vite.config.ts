40e3a8a6a2c545c3ec2fd3bc78275956f5019f3e
Aidan Bleser <aidanbleser35@gmail.com>
2025-09-16 06:56:44 -0500

    Update vite.config.ts

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,3 +1,6 @@
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import transformLucideImports from "@vite-plugin-transform-lucide-imports/svelte";
 
 export default defineConfig({
 	plugins: [
